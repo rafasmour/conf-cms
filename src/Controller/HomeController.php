9a6119798e0593c5869badfa17a9b796dfc5a87c
rafasmour <rafasmour@gmail.com>
2025-01-06 15:57:04 +0200

    updated .gitignore

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,14 +2,15 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HomeController
+class HomeController extends AbstractController
 {
     #[Route('/')]
     public function index()
     {
-        return new Response("Hello from a controller");
+        return $this->render('home/index.html.twig');
     }
 }
