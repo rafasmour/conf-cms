3358e6d9d7cb35d692765563cd7233fe8ddcad42
rafasmour <rafasmour@gmail.com>
2025-01-07 08:17:55 +0200

    created several routes

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -6,9 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\ProductRepository;
+use App\Entity\Product;
 class ProductController extends AbstractController
 {
-    #[Route('/products', name: 'product_index')]
+    #[Route('/product', name: 'product_index')]
     public function index(ProductRepository $repository): Response
     {
 
@@ -16,5 +17,16 @@ class ProductController extends AbstractController
             'controller_name' => 'ProductController',
             'products' => $repository->findAll(),
         ]);
+    }
+    #[Route('/product/{id<\d+>}', name: 'product_show')]
+    public function show(Product $product): Response
+    {
+        return $this->render('product/show.html.twig', [
+            'product' => $product
+        ]);
+    }
+    #[Route('/product/new', name: 'product_new')]
+    public function new(): Response{
+        return $this->render('product/new.html.twig');
     }
 }
