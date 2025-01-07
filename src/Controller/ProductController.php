360ff5d5371f2ce00e70d3c2a285ed3177bbc3bb
rafasmour <rafasmour@gmail.com>
2025-01-07 07:52:25 +0200

    loading products with migration and product repository

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,14 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-
+use App\Repository\ProductRepository;
 class ProductController extends AbstractController
 {
     #[Route('/products', name: 'product_index')]
-    public function index(): Response
+    public function index(ProductRepository $repository): Response
     {
+
         return $this->render('product/index.html.twig', [
             'controller_name' => 'ProductController',
+            'products' => $repository->findAll(),
         ]);
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,13 +4,27 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Product;
+
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $product = new Product();
+        $product->setName("Product One");
+        $product->setDescription("This is the first product");
+        $product->setSize(100);
+
+
+        $manager->persist($product);
+        $product = new Product();
+        $product->setName("Product Two");
+        $product->setDescription("this is the Second product");
+        $product->setSize(200);
+
+        $manager->persist($product);
+        
 
         $manager->flush();
     }
